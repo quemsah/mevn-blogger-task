a2da10cd234f7c5608d437ec98237379068c0d5f
evpavlov <evgenii.pavlov@t-systems.com>
2019-11-13 17:20:10 +0300

    Created a route and view for posts

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Posts from '@/components/Posts'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
+      path: '/helloworld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Posts',
+      component: Posts
     }
   ]
 })
